c3e3045f20199c1435e777fc4e1cce1b466f4a47
Maksim <mbrazhnikov1997@gmail.com>
2021-03-23 23:08:47 +0400

    FirstTry

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
-let number = 5;
-const leftborderwidth = 1;
-alert("Hello");
-number = 10;
-console.log(number);
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ""),
+      b = prompt('На сколько оцените его?', ""),
+      c = prompt('Один из последних просмотренных фильмов?', ""),
+      d = prompt('На сколько оцените его?', "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
